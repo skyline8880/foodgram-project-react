b9cab3e848f0c8056464a91da5fde2033b97c389
Saifulloi <skyline89_88@mail.ru>
2022-07-10 18:32:29 +0300

    fix_ver_04

--- backend/recipes/routers.py
+++ backend/recipes/routers.py
@@ -2,7 +2,6 @@ from rest_framework.routers import DefaultRouter
 
 from .views import RecipeViewSet, TagViewSet
 
-
 router_recipes = DefaultRouter()
 router_recipes.register('tags', TagViewSet, basename='tags')
 router_recipes.register('recipes', RecipeViewSet, basename='recipes')
