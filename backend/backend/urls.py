e66760598f373e65c91a873d4a47dae12eebaa61
Saifulloi <skyline89_88@mail.ru>
2022-07-17 00:29:08 +0300

    fix_ver_15

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,8 +4,6 @@ from django.contrib import admin
 from django.urls import include, path
 from ingredients.routers import router_ingredients
 from recipes.routers import router_recipes
-#from rest_framework_simplejwt.views import (TokenObtainPairView,
-#                                            TokenRefreshView)
 from users.routers import router_users
 from users.views import APILogoutView
 
@@ -17,12 +15,6 @@ urlpatterns = [
     path('api/auth/', include('djoser.urls')),
     path('api/auth/', include('djoser.urls.jwt')),
     path('api/auth/jwt/logout/', APILogoutView.as_view(), name='logout'),
-#    path('api/auth/token/login/',
-#         TokenObtainPairView.as_view(), name='token_obtain_pair'),
-#    path('api/auth/token/refresh/',
-#         TokenRefreshView.as_view(), name='token_refresh'),
-#    path('api/auth/token/logout/',
-#         APILogoutView.as_view(), name='logout'),
 ]
 
 if settings.DEBUG:
